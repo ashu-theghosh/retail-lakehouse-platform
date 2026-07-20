585121bc2c441e0d05812003602bdbaff73a39b1
ashu-theghosh <coldaghosh@gmail.com>
2026-07-20 18:29:43 +0000

    commit

--- framework/business_rule_validator.py
+++ framework/business_rule_validator.py
@@ -16,3 +16,54 @@ def erp_validator():
     col("status").isin("ACTIVE", "INACTIVE") &
     col("created_date").isNotNull()
     )
+
+def crm_validator():
+    return (
+        col("customer_id").isNotNull() &
+        col("email").isNotNull() &
+        col("address.city").isNotNull() &
+        col("address.pincode").isNotNull() &
+        col("address.state").isNotNull() &
+        col("name.first_name").isNotNull() &
+        col("name.last_name").isNotNull() &
+        col("phone").isNotNull() &
+        col("registration_date").isNotNull()
+        )
+    
+
+def website_validator():
+    return (
+        col("customer_id").isNotNull() &
+        (size(col("items")) > 0) &
+        col("order_id").isNotNull() &
+        col("order_status").isNotNull() &
+        col("order_timestamp").isNotNull() &
+        col("shipping_address.city").isNotNull() &
+        col("shipping_address.pincode").isNotNull() &
+        col("shipping_address.state").isNotNull() &
+        col("payment_amount").isNotNull()
+         )
+    
+
+def inventory_validator():
+    return (
+      col("products").isNotNull() &
+      (size(col("products"))>0) &
+      col("warehouse_id").isNotNull() &
+      col("warehouse_name").isNotNull()
+       )
+    
+
+
+def payment_validator():
+    return (
+       col("amount").isNotNull() &
+       col("customer_id").isNotNull() &
+       col("order_id").isNotNull() &
+       col("payment_id").isNotNull() &
+       col("payment_method").isNotNull() &
+       col("payment_timestamp").isNotNull() &
+       col("refund.eligible").isin([True, False]) &
+       col("refund.refund_status").isNotNull() &
+       col("status").isNotNull()
+       )
--- framework/mysql_connector.py
+++ framework/mysql_connector.py
@@ -21,10 +21,47 @@ def mysql_conn():
         conn.close()
 
 
-def fetch_eligible_table():
+def fetch_eligible_pipelines():
     with mysql_conn() as conn:
         cur=conn.cursor(sql.cursors.DictCursor)
         cur.execute("select * from pipeline_config where active_flag=1")
         rows=cur.fetchall()
         cur.close()
     return rows
+
+
+
+'''def insert_pipeline_execution(pipeline_name=None,status,total_records=None,good_records=None,bad_records=None,error_message=None):
+    with mysql_conn() as conn:
+        cur=conn.cursor()
+        if status=="RUNNING":
+            cur.execute(f"insert into pipeline_execution(pipeline_name,status,start_time) values(%s,%s,NOW())")
+        elif status=="COMPLETED":
+            cur.execute(f"insert into pipeline_execution(status,end_time,total_records,good_records,bad_records) values(%s,NOW(),%s,%s,%s)")
+        elif status=="FAILED":
+            cur.execute(f"insert into pipeline_execution(status,end_time,total_records,good_records,bad_records,error_message) values(%s,NOW(),%s,%s,%s,%s)")'''
+
+
+
+def insert_execution(pipeline_name):
+    with mysql_conn() as conn:
+        cur=conn.cursor()
+        cur.execute("insert into pipeline_execution(pipeline_name,status,start_time) values(%s,%s,NOW())",(pipeline_name,"RUNNING"))
+        batch_id=cur.lastrowid
+        conn.commit()
+        cur.close()
+    return batch_id
+
+def update_completed(batch_id,total_records,good_records,bad_records):
+    with mysql_conn() as conn:
+        cur=conn.cursor()
+        cur.execute("update pipeline_execution set status=%s,end_time=NOW(),total_records=%s,good_records=%s,bad_records=%s where batch_id=%s",("COMPLETED",total_records,good_records,bad_records,batch_id))
+        conn.commit()
+        cur.close()
+
+def update_failed(batch_id,total_records,good_records,bad_records,error_message):
+    with mysql_conn() as conn:
+        cur=conn.cursor()
+        cur.execute("update pipeline_execution set status=%s,end_time=NOW(),total_records=%s,good_records=%s,bad_records=%s,error_message=%s where batch_id=%s",("FAILED",total_records,good_records,bad_records,error_message,batch_id))
+        conn.commit()
+        cur.close()
